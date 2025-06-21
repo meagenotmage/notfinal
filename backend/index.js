5f8b1927a292435e2d2df3adf7cd741eb08a2434
meagenotmage <meagelleinerose.sionosa@wvsu.edu.ph>
2025-06-22 02:11:46 +0800

    Change again the database

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err));
 
 app.use('/api/announcements', announcementsRoutes);
 
